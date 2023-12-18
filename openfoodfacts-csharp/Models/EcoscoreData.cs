d208a5dfdc6a242f8e42436661a3cea3aaf7ee0f
Jean-Pierre Dupont <jprdupont@gmail.com>
2023-12-18 15:31:31 +0000

    Adds parts of EcoscoreData to api
    With the purpose of accessing some of the recycling data

---
+++ openfoodfacts-csharp/Models/EcoscoreData.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Newtonsoft.Json;
+
+namespace OpenFoodFactsCSharp.Models
+{
+    public class EcoscoreData
+    {
+        [JsonProperty("adjustments")]
+        public Adjustments Adjustments { get; set; }
+    }
+}
